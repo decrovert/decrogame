ba4fd3c4e1188e71b5f04e1c0a634267ac49e96e
decrovert <decrovertp@gmail.com>
2022-07-14 20:17:18 +0100

    setting up project

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "Decrogame"
+    architecture "x86_64"
+	
+    configurations {
+        "Debug",
+        "Release"
+    }
+
+    project "Decrogame"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++20"
+        
+        targetdir "%{prj.location}/bin/"
+        objdir "%{prj.location}/obj/"
+        
+        files "src/**.cpp"
